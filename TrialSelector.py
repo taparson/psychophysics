eaf360227f05fa79ec9d07cac1242b0d4f4751db
taparson <timothy_parsons@brown.edu>
2014-11-18 11:53:27 -0500

    trials work now with two hardcoded location arrays, errors out after 48 trials though

--- TrialSelector.py
+++ TrialSelector.py
@@ -3,7 +3,17 @@ from random import randrange
 
 def choose():
     control = logic.getCurrentController()
-    string = 'VisA1'
-    trial = control.actuators[string]
-    control.activate(control.actuators['Edit Object'])
-    control.activate(trial)
+    #string = 'VisA1'
+    #trial = control.actuators[string]
+    #control.activate(control.actuators['Edit Object'])
+    #control.activate(trial)
+    # get the current scene
+    scene = logic.getCurrentScene()
+    
+    # and use it to get a list of the objects in the scene
+    objList = scene.objects
+    
+    objList["Bounds"].position = [0,5,-3]
+    for i in range(7):
+        curr = objList["Cylinder" + str(i+1)]
+        curr.visible = True
